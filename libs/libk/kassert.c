9d7a78071b7d8c8c1926ab44d33504fedd093bf7
V01D-NULL <timster23@protonmail.com>
2021-07-10 14:39:11 +0200

    Fix bug in assertion function

--- libs/libk/kassert.c
+++ libs/libk/kassert.c
@@ -2,9 +2,9 @@
 #include "common.h"
 #include "drivers/gfx/gfx.h"
 
-void _assert(const char *file, uint64_t line, const char* func, const char *desc) {
+void __assert_impl(const char *file, uint64_t line, const char* func, const char *desc) {
     gfx_set_colors(0xF70D1A, 0x0);
-    printk("%s:%d: %s: Assertion `%s' failed", (char*)file, line, func, desc);
+    printk("ASSERT", "%s:%d: %s: Assertion `%s' failed", (char*)file, line, func, desc);
 
     for (;;)
         asm ("hlt");
--- libs/libk/kassert.h
+++ libs/libk/kassert.h
@@ -14,13 +14,15 @@
 
 #include <stdint.h>
 
-void _assert(const char *file, uint64_t line, const char* func, const char *desc);
+void __assert_impl(const char *file, uint64_t line, const char* func, const char *desc);
 
 /**
  * @brief Simple assertion function.
  * 
  * @param[in] x 'x' is a logical expression. It fails the assertion if x is not true, if x is true however, the assertion succeeds
  */
-#define assert(x)  x ? (int) 0 : _assert(__FILE__, __LINE__, __FUNCTION__, #x)
+#define assert(x) ({ (x) ? 0 : __assert_impl(__FILE__, __LINE__, __FUNCTION__, #x); })
+
+
 
 #endif // LIB_KERNEL_ASSERT_H
