165ecd1131796612bc8dca9cd51ae71de9c14009
V01D-NULL <timster23@protonmail.com>
2021-10-14 22:38:31 +0200

    Log assertion failure to serial port

--- libs/libk/kassert.c
+++ libs/libk/kassert.c
@@ -1,11 +1,13 @@
 #include "kassert.h"
+#include <drivers/io/serial.h>
 #include <util/common.h>
 #include <printk.h>
 
 void __assert_impl(const char *file, uint64_t line, const char *func, const char *desc)
 {
     printk("ASSERT", "%s:%d: %s: Assertion `%s' failed", (char *)file, line, func, desc);
-
+    debug(true, "ASSERT", "%s:%d: %s: Assertion `%s' failed", (char *)file, line, func, desc);
+    
     for (;;)
         __asm__("hlt");
 }
