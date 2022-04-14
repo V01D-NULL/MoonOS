734b53fef264668238b9fc01a8c42c8c85d9ffc9
V01D-NULL <timster23@protonmail.com>
2022-04-14 17:22:37 -0500

    daemon_load: swapgs before jumping to ring3

--- kernel/proc/daemon/load.c
+++ kernel/proc/daemon/load.c
@@ -5,11 +5,12 @@
 void load_daemon(const uint8_t *elf, const char *name)
 {
     // Note: identity_map = true basically assumes that the load address of the ELF is free, this might be removed in the future.
-    task_t task = load_elf(elf, (struct elf_loader_args) {.do_panic = true, .identity_map = true, .descriptor = name});
-    
+    task_t task = load_elf(elf, (struct elf_loader_args){.do_panic = true, .identity_map = true, .descriptor = name});
+
     if (!task.entrypoint)
         panic("Failed to load ELF; Cannot start kernel daemon!");
-    
-	switch_pagemap(task);
-	enter_ring3((void *)task.entrypoint, task.ustack);
+
+    asm("swapgs");
+    switch_pagemap(task);
+    enter_ring3((void *)task.entrypoint, task.ustack);
 }
