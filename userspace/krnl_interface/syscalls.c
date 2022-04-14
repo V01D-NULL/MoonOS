c26e95ade5ccfedde81c2205b54ea83ab1ddf2a7
V01D-NULL <timster23@protonmail.com>
2022-04-14 17:25:10 -0500

    libs/userspace -> userspace/

--- userspace/krnl_interface/syscalls.c
+++ userspace/krnl_interface/syscalls.c
@@ -1,12 +1,17 @@
 #include "syscalls.h"
-// #include <libk/kprintf.h>
-#include <stdint.h>
+
+// Todo....
+// Also, take note of how this needs to be setup in order to port mlibc which should be done asap
+void syscall(void)
+{
+    asm volatile(
+        "syscall\n"
+    );
+}
 
 // Note: The string should already be formatted when calling syscall_log()
 // meaning it does NOT have printf() functionality.
 void syscall_log(const char *msg)
 {
-    asm volatile(
-        "syscall\n"
-    );
+    syscall();
 }
