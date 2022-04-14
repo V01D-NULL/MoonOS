414489747eacb70f9d49885d77d22ade9c52f2a4
V01D-NULL <timster23@protonmail.com>
2022-04-14 17:23:19 -0500

    elf: Simplfy elf loading/mapping

--- kernel/proc/elf/elf.c
+++ kernel/proc/elf/elf.c
@@ -38,9 +38,10 @@ static task_t elf_parse_phdr(const uint8_t **elf, Elf64_Ehdr *ehdr, struct elf_l
     Elf64_Phdr *phdr = (Elf64_Phdr *)(*elf + ehdr->e_phoff);
     task_t task = new_task(args.descriptor, ehdr->e_entry);
 
+    size_t elf_mapping_offset = phdr->p_vaddr;
     bool found_ptload = false;
     copy_kernel_mappings(task);
-
+    
     for (Elf64_Half i = 0; i < phdr_entries; i++)
     {
         if (phdr->p_type == PT_LOAD)
@@ -48,20 +49,15 @@ static task_t elf_parse_phdr(const uint8_t **elf, Elf64_Ehdr *ehdr, struct elf_l
             found_ptload = true;
             size_t num_pages = ALIGN_UP(phdr->p_memsz) / 4096;
 
-            // printk("elf", "Virtual mapping: 0x%lX (%d bytes | %d pages)\n", phdr->p_vaddr, phdr->p_memsz, num_pages);
+            printk("elf", "Virtual mapping: 0x%lX (%d bytes | %d pages)\n", phdr->p_vaddr, phdr->p_memsz, num_pages);
 
-            // Todo: This part is super hacky, ugly and totally bug prone but it works for now. Fix it later.
-            size_t elf_base = args.identity_map ? phdr->p_vaddr : pmm_alloc_range(num_pages).base; // Todo: Check if p_vaddr is really a physical address and not a higher half one. (Or just remove all this garbage when threads and scheduling are implemented)
             for (uint64_t i = 0; i < num_pages; i++)
             {
-                v_map(task.pagemap, phdr->p_vaddr + (i * PAGE_SIZE), elf_base + (i * PAGE_SIZE), MAP_USER_RO);
+                elf_mapping_offset += (i * PAGE_SIZE);
+                v_map(task.pagemap, elf_mapping_offset, elf_mapping_offset, MAP_USER_RW);
             }
 
-            if (args.identity_map)
-                memcpy((uint8_t *)phdr->p_vaddr, (const uint8_t *)*elf + phdr->p_offset, phdr->p_filesz);
-            else
-                memcpy((uint8_t *)elf_base, (const uint8_t *)*elf + phdr->p_offset, phdr->p_filesz);
-            
+            memcpy((uint8_t *)phdr->p_vaddr, (const uint8_t *)*elf + phdr->p_offset, phdr->p_filesz);            
             memset((void *)phdr->p_vaddr + phdr->p_filesz, 0, phdr->p_filesz - phdr->p_memsz);
         }
 
@@ -74,6 +70,14 @@ static task_t elf_parse_phdr(const uint8_t **elf, Elf64_Ehdr *ehdr, struct elf_l
         return (task_t){};
     }
 
-    v_map_range(as_vmm_range(task.ustack, task.ustack + 8192, $identity_vma), MAP_USER_RW, task.pagemap);
+    // Stack memory (8KiB)
+    task.ustack = elf_mapping_offset;
+    for (size_t i = 0; i <= 2; i++)
+    {
+        auto addr = i * PAGE_SIZE + task.ustack;
+        v_map(task.pagemap, addr, addr, MAP_USER_RW);
+    }
+    // Todo: Shadow stack page
+
     return task;
 }
