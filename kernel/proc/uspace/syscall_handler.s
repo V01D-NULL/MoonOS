f9511de0e05c37a8ac7208cb11ed73096852d7ed
V01D-NULL <timster23@protonmail.com>
2022-04-14 17:27:42 -0500

    kernel_syscalls: Setup user segment selectors before returning to ring3

--- kernel/proc/uspace/syscall_handler.s
+++ kernel/proc/uspace/syscall_handler.s
@@ -5,9 +5,19 @@ extern syscall_handler
 global x86_syscall_handler
 x86_syscall_handler:
     swapgs
+    
     push rcx ; Don't clobber rcx- It holds return address.
     mov rdi, [gs:0x0]
     call syscall_handler
     pop rcx
     swapgs
+
+    ; NOTE: I'm not setting SS or CS as these are automatically set by sysret
+    ; Set user segment selectors
+    mov ax, 0x3B
+    mov ds, ax
+    mov fs, ax
+    mov gs, ax
+    mov es, ax
+
     o64 sysret
--- kernel/proc/uspace/uspace.s
+++ kernel/proc/uspace/uspace.s
@@ -10,6 +10,14 @@ enter_ring3_sysret:
     mov rbp, rsi
     mov rsp, rbp
 
+    ; NOTE: I'm not setting SS or CS as these are automatically set by sysret
+    ; Set user segment selectors
+    mov ax, 0x3B
+    mov ds, ax
+    mov fs, ax
+    mov gs, ax
+    mov es, ax
+
     ; Enter ring3
     mov rcx, rdi   ; RIP
     mov r11, 0x202 ; RFLAGS
