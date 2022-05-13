677872789bfbb162ec505f26cc93f0bcecefb750
V01D-NULL <timster23@protonmail.com>
2022-05-13 18:35:24 -0500

    printk: Improve printing routine

--- kernel/printk.c
+++ kernel/printk.c
@@ -6,7 +6,6 @@
 #include <devices/term/limine-port/term.h>
 #include <mm/mm.h>
 
-char buffer[512];
 static bool is_verbose_boot = false;
 
 void printk_init(bool verbose_boot, BootContext term_info)
@@ -18,6 +17,7 @@ void printk_init(bool verbose_boot, BootContext term_info)
 
 void printk(char *status, char *fmt, ...)
 {
+    char buffer[512];
     va_list arg;
     va_start(arg, fmt);
     vsnprintf((char *)&buffer, (size_t)-1, fmt, arg);
@@ -28,22 +28,17 @@ void printk(char *status, char *fmt, ...)
         serial_set_color(BASH_GREEN);
         debug(false, "DEBUG: ");
         serial_set_color(BASH_DEFAULT);
-        debug(false, "[%s] %s", status, buffer);
+        debug(false, "[%s] %s", status, (const char *)&buffer);
         return;
     }
 
-    // _term_write(status, strlen(status));
-    // _term_write(": ", 2);
-    // for (size_t i = 0, n = strlen(buffer); i < n; i++)
-        // _gterm_putchar(buffer[i]);
 
-    // gterm_putchar('A');
-    // gterm_double_buffer_flush();
-    _term_write((const char *)&buffer, strlen(buffer));
+    fmt_puts("%s: %s", status, buffer);
 }
 
 void fmt_puts(const char *fmt, ...)
 {
+    char buffer[512];
     va_list arg;
     va_start(arg, fmt);
     vsnprintf((char *)&buffer, (size_t)-1, fmt, arg);
