0fdbca6fc7a6474ae1fa6812e1a837e26b6127b0
V01D-NULL <timster23@protonmail.com>
2024-08-23 14:33:09 +0200

    chore: include stdalign.h in base/aligh.h

--- libs/base/align.h
+++ libs/base/align.h
@@ -1,15 +1,16 @@
 #ifndef BASE_ALIGN_H
 #define BASE_ALIGN_H
 
-#include <mm/page.h>
 #include <base/base-types.h>
+#include <mm/page.h>
+#include <stdalign.h>
 
-	#define ALIGN_UP(__number) (((__number) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define ALIGN_UP(__number) (((__number) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
 #define ALIGN_DOWN(__number) ((__number) & ~(PAGE_SIZE - 1))
 
 inline uint64_t align(int size, int align)
 {
-	return (size + align - 1) & ~(align - 1);
+    return (size + align - 1) & ~(align - 1);
 }
 
-#endif // BASE_ALIGN_H
+#endif  // BASE_ALIGN_H
