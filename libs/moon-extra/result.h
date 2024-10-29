fc4b62c8d76ff2ce6fb1c91f7a0a18e9d97c8adb
Mystical <timster23@protonmail.com>
2024-10-29 13:56:00 +0100

    chore: add UNWRAP, TRY_UNWRAP to result.h

--- libs/moon-extra/result.h
+++ libs/moon-extra/result.h
@@ -30,4 +30,24 @@ typedef void *Nullish;
         .is_ok = true, .ok = V \
     }
 
+#define UNWRAP(fn)                                              \
+    ({                                                          \
+        auto result = fn;                                       \
+        if (!result.is_ok)                                      \
+        {                                                       \
+            panic("Unwrapping error result! (%s)", result.err); \
+        }                                                       \
+        result.ok;                                              \
+    })
+
+#define TRY_UNWRAP(fn, T)                \
+    ({                                   \
+        auto result = fn;                \
+        if (!result.is_ok)               \
+        {                                \
+            return Error(T, result.err); \
+        }                                \
+        result.ok;                       \
+    })
+
 #endif  // RESULT_H
